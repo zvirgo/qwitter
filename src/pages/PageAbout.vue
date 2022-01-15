99f9af4e1182b3d24e34bd21b69c79ccf102cbef
zeinali <zeinali.b69@yahoo.com>
2022-01-15 16:34:50 +0330

    add content to about page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,5 +1,21 @@
 <template>
-  <q-page><h5>About</h5> </q-page>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body">
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero
+        nesciunt maiores eum est unde architecto sequi nulla itaque vitae in
+        doloribus nemo, nam alias repellendus et atque cupiditate esse
+        inventore?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eveniet
+        quam id sequi libero! Consectetur deleniti qui dolores deserunt,
+        veritatis doloremque magnam quis a voluptates suscipit eos, dolore
+        recusandae iusto?
+      </p>
+    </div>
+  </q-page>
 </template>
 
 <script>
