6f41947bcffb9dd642e528082ca9f8a2e4edefbf
zeinali <zeinali.b69@yahoo.com>
2022-01-16 09:06:38 +0330

    add input for newQweet

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,36 @@
 <template>
-  <q-page><h5>Home</h5> </q-page>
+  <q-page>
+    <div class="row items-end q-col-gutter-md q-py-lg q-px-md">
+      <div class="col">
+        <q-input
+          bottom-slots
+          v-model="newQweet"
+          placeholder="what's happening"
+          counter
+          autogrow
+          maxlength="280"
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img src="https://cdn.quasar.dev/img/avatar5.jpg" />
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          unelevated
+          rounded
+          no-caps
+          :disable="!newQweet"
+          color="primary"
+          label="Qweet"
+          class="q-mb-lg"
+        />
+      </div>
+    </div>
+    <q-separator  size="10px" color="grey-2" />
+  </q-page>
 </template>
 
 <script>
@@ -7,5 +38,10 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PageHome",
+  data() {
+    return {
+      newQweet: "",
+    };
+  },
 });
 </script>
